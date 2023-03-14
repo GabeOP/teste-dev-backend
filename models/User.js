7147e9f81736fb3b22da95df66127fb392d143a5
GabeOP <gabrieloliveirap1@hotmail.com>
2023-03-14 13:47:51 -0300

    field change

--- models/User.js
+++ models/User.js
@@ -2,16 +2,13 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: String,
+  email: String,
   birth_date: String,
   sex: String,
-  health_problems: [
-    { 
-      problem_name: String, 
-      degree_problem: { 
-        type: String, max: 2, min: 1 
-      } 
-    },
-  ],
+  health_problems: { 
+    problem_name: String, 
+    problem_level: { type: Number, max: 2, min: 1 } 
+  },
   createdIn: {type: Date, default: Date.now},
   updated: Date
 });
