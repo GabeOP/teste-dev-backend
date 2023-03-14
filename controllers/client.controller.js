b226f5f9e3554822973e44815df7d947491296cd
GabeOP <gabrieloliveirap1@hotmail.com>
2023-03-14 13:48:37 -0300

    add methods HTTP

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,7 +1,52 @@
-
+const User = require("../models/User");
 
 module.exports = {
+
+  get: async(req, res)=>{
+    const find = await User.find()
+    res.status(200).json(find)
+  },
+  getByEmail: async(req, res)=>{
+    const email = req.params;
+
+    const emailExists = await User.findOne(email);
+    if(!emailExists){
+      res.status(404).json({message: "User not found"})
+    }
+
+    res.status(200).json(emailExists)
+  },
   post: async(req, res)=>{
+    const {name, email, birth_date, sex, health_problems, updated} = req.body;
     
+    if(!name|| !email|| !birth_date|| !sex|| !updated){
+      return res.status(422).json({message: "complete all fields"})
+    }
+
+    const emailExists = await User.findOne({email: email})
+    if(emailExists){
+      return res.status(422).json({message: "e-mail is already registered"})
+    }
+
+    const user = new User({name, email, birth_date, sex, health_problems, updated});
+
+    try {
+      await user.save();
+      res.status(201).json({message: "User created successfully"})
+    } catch (error) {
+      
+    }
+  },
+  patch: async(req, res)=>{
+    const id = req.params;
+    const {name, email, birth_date, sex, health_problems, updated} = req.body;
+    
+    const emailExists = await User.findOne({email: email})
+    if(emailExists){
+      return res.status(422).json({message: "e-mail is already registered"})
+    }
+    
+    await User.findByIdAndUpdate(id, {name, email, birth_date, sex, health_problems, updated})
+    res.status(200).json({message: "editado"})
   }
 }
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const Controller = require("./controllers/client.controller");
+app.get("/", Controller.get)
+app.get("/:email", Controller.getByEmail)
 app.post("/", Controller.post)
+app.patch("/:_id", Controller.patch)
 
 const connectDb = require("./database/db");
 connectDb();
